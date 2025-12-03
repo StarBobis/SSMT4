b37b5e3bdcfd4ee9a4d6a96d44bbb0f5174ba937
Administrator <Administrator@DESKTOP-BBNRNSR>
2025-12-03 10:03:01 +0800

    添加设置窗口标题，以及窗口居中的Rust代码

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Manager;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +10,15 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            let version = &app.package_info().version;
+            window.set_title(&format!("SSMT4 v{}", version)).unwrap();
+            window.set_size(tauri::Size::Logical(tauri::LogicalSize { width: 1000.0, height: 671.0 })).unwrap();
+            window.set_resizable(true).unwrap();
+            window.center().unwrap();
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
